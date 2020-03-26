ae421784188b505e0ad8a2c8cf47cced3a011f93
Nikolay1998 <kriverson13@gmail.com>
2020-03-26 15:49:50 +0400

    Just Hello World

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TrackListComponent } from './track-list/track-list.component';
+import { GenreListComponent } from './genre-list/genre-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackListComponent,
+    GenreListComponent
   ],
   imports: [
     BrowserModule
