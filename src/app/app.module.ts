3246d0731accc1227644bbc0cf7c2265ad8ecbb4
Nikolay1998 <kriverson13@gmail.com>
2020-03-26 16:03:13 +0400

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TrackListComponent } from './track-list/track-list.component';
 import { GenreListComponent } from './genre-list/genre-list.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,11 @@ import { GenreListComponent } from './genre-list/genre-list.component';
     GenreListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'tracks', component: TrackListComponent},
+      {path: 'genres', component: GenreListComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/entity/genre.ts
@@ -0,0 +1,11 @@
+export const genre = [
+  {
+    title: 'Genre1',
+  },
+  {
+    title: 'Genre2',
+  },
+  {
+    title: 'Genre3',
+  }
+];
