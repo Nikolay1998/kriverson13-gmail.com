6a235add1c015169147fa1dab7d0840b625e5d9f
Nikolay1998 <kriverson13@gmail.com>
2020-04-07 18:41:35 +0400

    package.json correct

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,3 +29,4 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('.content span').textContent).toContain('aa app is running!');
   });
 });
+
--- src/app/track-list/track-list.component.spec.ts
+++ src/app/track-list/track-list.component.spec.ts
@@ -23,3 +23,5 @@ describe('TrackListComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
